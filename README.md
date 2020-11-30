40df24eb2fddc910d44d03c75ce78e9648c03e43
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-29 22:22:24 -0600

    Getting project set up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AirSim-Python
-Scripts I've been using to work with AirSim
+1. Clone this repository
+2. Create your virtual environment with 'python -m venv venv'
+3. Activate your virtual environment
+    Windows: .\venv\Scripts\activate
+    Mac: ./venv/source/activate
+4. pip install .
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='AirSim-Python',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/dbaldwin/AirSim-Python',
+    license='MIT',
+    author='Dennis Baldwin',
+    author_email='',
+    description='AirSim API examples written in Python',
+    install_requires = [
+        'numpy',
+        'airsim'
+    ]
+
+)
