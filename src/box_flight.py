f56b8b38d5acd1c1b2df86b7c1d14a619e1e711f
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-11-29 22:22:41 -0600

    Different heading modes tested with Blocks environment

---
+++ src/box_flight.py
@@ -0,0 +1,63 @@
+import airsim
+import sys
+import time
+
+client = airsim.MultirotorClient()
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
+client.takeoffAsync().join()
+
+# AirSim uses NED coordinates so negative axis is up.
+# -10 meters above the original launch point.
+z = -10
+
+# Fly given velocity vector for 5 seconds
+duration = 5
+
+# Box with nose pointed towards heading
+client.moveByVelocityZAsync(5, 0, z, duration, airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(0, 5, z, duration, airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(-5, 0, z, duration, airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(0, -5, z, duration, airsim.DrivetrainType.ForwardOnly, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+# Reset heading
+client.rotateToYawAsync(0)
+time.sleep(2)
+
+# Box with nose pointed forward the whole time
+client.moveByVelocityZAsync(5, 0, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(0, 5, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(-5, 0, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(0, -5, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+# Box with nose pointed towards the inside of the box
+client.moveByVelocityZAsync(5, 0, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 90)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(0, 5, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 180)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(-5, 0, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 270)).join()
+time.sleep(2)
+
+client.moveByVelocityZAsync(0, -5, z, duration, airsim.DrivetrainType.MaxDegreeOfFreedom, airsim.YawMode(False, 0)).join()
+time.sleep(2)
+
+# Land
+time.sleep(2)
+client.landAsync().join()
