b6ccd3a7037ecd5ca6ef4993c9bd73099734eeea
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-12-24 08:56:59 -0600

    Fully autonomous

--- src/detect_aruco_marker.py
+++ src/detect_aruco_marker.py
@@ -2,26 +2,24 @@ import airsim
 import sys
 from threading import Thread # So we can get the camera feed and send commands in parallel
 from aruco.ArucoUtils import ArucoUtils
+import time
 
 # Establish connection with AirSim
 client = airsim.MultirotorClient()
-# client.confirmConnection()
-# client.enableApiControl(True)
-# client.armDisarm(True)
+client.confirmConnection()
+client.enableApiControl(True)
+client.armDisarm(True)
 
 # Takeoff
-#client.takeoffAsync().join()
-
-temp = ArucoUtils(client, "scene")
-temp.getVideoFrame()
-
-#print(temp.getFrame())
+client.takeoffAsync().join()
 
 # Begin the video thread
-# videoThread = Thread(target=getVideoFrame)
-# videoThread.start()
+aruco = ArucoUtils(client, "scene")
+videoThread = Thread(target=aruco.getVideoFrame)
+videoThread.start()
 
-# time.sleep(1)
+# Pause before we go
+time.sleep(1)
 
 # Fly to the ArUco Tower
-#client.moveToPositionAsync(220, -15, 35, 10)
+client.moveToPositionAsync(220, -15, 35, 10)
