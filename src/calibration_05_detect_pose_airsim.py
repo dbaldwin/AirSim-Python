2982a21c428cddaf373fef341ab5992e0df68f5e
Dennis Baldwin <dennisbaldwin@gmail.com>
2020-12-26 12:45:32 -0600

    Cleanup

--- src/calibration_05_detect_pose_airsim.py
+++ src/calibration_05_detect_pose_airsim.py
@@ -48,38 +48,7 @@ while True:
         # Show the pose
         cv2.imshow('Pose', frame)
 
+    # Exit on escape or q key
     key = cv2.waitKey(1) & 0xFF
     if (key == 27 or key == ord('q')):
         break
-
-# aruco = ArucoUtils()
-# videoThread = Thread(target=aruco.getVideoFrame(client, 'scene'))
-# videoThread.start()
-
-# # Loop over the calibration images and detect markers
-# for subdir, dirs, files in os.walk('.\src\calibration\images'):
-
-#     for file in files:
-
-#         # Read into a variable
-#         frame = cv2.imread(subdir + '\\' + file)
-
-#         # Covert to grayscale
-#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-#         # Detect the markers
-#         corners, ids, rejected = aruco.detectMarkers(gray, aruco_dict)
-
-#         if ids is not None:
-
-#             # rvecs and tvecs are the rotation and translation vectors for each of the markers in corners.
-#             rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 1, cameraMatrix, distCoeffs)
-
-#             for rvec, tvec in zip(rvecs, tvecs):
-#                 aruco.drawAxis(frame, cameraMatrix, distCoeffs, rvec, tvec, 1)
-
-#         # Display the pose
-#         cv2.imshow('frame', frame)
-
-#         # Pause before the next frame
-#         cv2.waitKey(3000)
