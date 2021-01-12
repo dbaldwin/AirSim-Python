8e6efc0c2822bf01641a0c4517a27b69610acdc3
Dennis Baldwin <dennisbaldwin@gmail.com>
2021-01-12 11:08:49 -0600

    Let's simplify for now where we don't need to take off to see the marker

--- src/detect_aruco_marker.py
+++ src/detect_aruco_marker.py
@@ -4,22 +4,22 @@ from threading import Thread # So we can get the camera feed and send commands i
 from aruco.ArucoUtils import ArucoUtils
 import time
 
-# Establish connection with AirSim
+set# Establish connection with AirSim
 client = airsim.MultirotorClient()
-client.confirmConnection()
-client.enableApiControl(True)
-client.armDisarm(True)
+#client.confirmConnection()
+#client.enableApiControl(True)
+#client.armDisarm(True)
 
 # Takeoff
-client.takeoffAsync().join()
+#client.takeoffAsync().join()
 
 # Begin the video thread
-aruco = ArucoUtils(client, "scene")
-videoThread = Thread(target=aruco.getVideoFrame)
+aruco = ArucoUtils()
+videoThread = Thread(target=aruco.getVideoFrameAndDrawMarkers(client, 'scene'))
 videoThread.start()
 
 # Pause before we go
-time.sleep(1)
+#time.sleep(3)
 
 # Fly to the ArUco Tower
-client.moveToPositionAsync(220, -15, 35, 10)
+#client.moveToPositionAsync(220, -15, 35, 10)
